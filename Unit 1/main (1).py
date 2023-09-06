f5413bc51490b83ebf2c89b9d29681c2deed344e
Far41031 <144271132+Far41031@users.noreply.github.com>
2023-09-06 23:52:47 +0530

    Add files via upload

---
+++ Unit 1/main (1).py
@@ -0,0 +1,15 @@
+def is_leap_year(year):
+    # Leap years are either divisible by 4 and not divisible by 100,
+    # or they are divisible by 400.
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+# Input a year from the user
+year = int(input("Enter a year: "))
+
+if is_leap_year(year):
+    print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
